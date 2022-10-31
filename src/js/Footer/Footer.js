26d7fdd0992596ae6b71fe9c29cdd16fd476ca40
niklander96 <96373865+niklander96@users.noreply.github.com>
2022-10-31 21:47:09 +0300

    Initialize project using Create React App

--- src/js/Footer/Footer.js
+++ src/js/Footer/Footer.js
@@ -1,34 +1,32 @@
 import './Footer.css';
-import {Component} from 'react';
+import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import TasksFilter from '../TaskFilter/TasksFilter';
 
 export default class Footer extends Component {
-	render() {
-		const {toDo, clearCompleted, changeRenderStatus} = this.props;
-		return (
-			<footer className="footer">
-				<span className="todo-count">{`${toDo} items left`}</span>
-				<TasksFilter changeRenderStatus={changeRenderStatus}/>
-				<button className="clear-completed" onClick={clearCompleted}>
-					Clear completed
-				</button>
-			</footer>
-		);
-	}
+  render() {
+    const { toDo, clearCompleted, changeRenderStatus } = this.props;
+    return (
+      <footer className="footer">
+        <span className="todo-count">{`${toDo} items left`}</span>
+        <TasksFilter changeRenderStatus={changeRenderStatus} />
+        <button className="clear-completed" onClick={clearCompleted}>
+          Clear completed
+        </button>
+      </footer>
+    );
+  }
 }
 
 Footer.defaultProps = {
-	toDo: 0,
-	changeRenderStatus: () => {
-	},
-	clearCompleted: () => {
-	},
+  toDo: 0,
+  changeRenderStatus: () => {},
+  clearCompleted: () => {},
 };
 
 Footer.propTypes = {
-	toDo: PropTypes.number,
-	changeRenderStatus: PropTypes.func,
-	clearCompleted: PropTypes.func,
+  toDo: PropTypes.number,
+  changeRenderStatus: PropTypes.func,
+  clearCompleted: PropTypes.func,
 };
