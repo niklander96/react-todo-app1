5a3cbee2583a4c76808c5ba203a960ed6a66d559
niklander96 <96373865+niklander96@users.noreply.github.com>
2022-10-31 19:10:32 +0300

    Initialize project using Create React App

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './js/App/App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render( <App/> );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
