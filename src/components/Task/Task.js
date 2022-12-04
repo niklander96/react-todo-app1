91186a2f7b28f045386a836a5e5bd35482a34f5c
Никита <nik.loktev151069@gmail.com>
2022-12-04 20:30:24 +0300

    Commit

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,66 +1,80 @@
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 
-export default class Task extends Component {
-  interval
-  state = {
-    isStarted: this.props.isStarted,
+const Task = ({ onDeleted, onCompleted, done, onEdited, title, id, seconds, minutes, date, getStart, getPause, isStarted, timeLeft }) => {
+  let interval
+  let timer
+  // state = {
+  //   isStarted: this.props.isStarted,
+  // }
+
+  // const [isStarted, setIsStarted] = useState('false')
+
+  const onStart = () => {
+    clearInterval(interval)
+    interval = setInterval(getStart, 1000)
   }
 
-  onStart = () => {
-    const { getStart } = this.props
-    clearInterval(this.interval)
-    this.interval = setInterval(getStart, 1000)
-  }
-
-  onPause = () => {
-    const { getPause } = this.props
+  const onPause = () => {
+    clearInterval(interval)
     getPause()
-    clearInterval(this.interval)
-  }
 
-  componentDidMount() {
-    const { getStart } = this.props
-    this.interval = setInterval(getStart, 1000)
-    const { timeLeft } = this.props
-    setInterval(timeLeft, 5000)
   }
+  // useEffect(() => {
+  //   timer = setInterval( timeLeft(), 5000)
+  //   return () => clearInterval(timer)
+  // })
 
-  componentDidUpdate(prevProps) {
-    if (this.props.getPause === prevProps.getPause) {
-      clearInterval(this.interval)
-    }
-  }
+  useEffect(() => {
+  interval = setInterval(() => isStarted && getStart(), 1000);
+  return () => clearInterval(interval)
+  }, [isStarted])
 
-  componentWillUnmount() {
-    const { timeLeft } = this.props
-    clearInterval(this.interval)
-    clearInterval(setInterval(timeLeft, 5000))
-  }
 
-  render() {
-    const { onDeleted, onCompleted, done, onEdited, title, id, seconds, minutes, date } = this.props
-    const sec = seconds.toString().padStart(2, '0')
-    const min = minutes.toString().padStart(2, '0')
+
+  // componentDidMount() {
+  //   const { getStart } = this.props
+  //   this.interval = setInterval(getStart, 1000)
+  //   const { timeLeft } = this.props
+  //   setInterval(timeLeft, 5000)
+  // }
+  //
+  // componentDidUpdate(prevProps) {
+  //   if (this.props.getPause === prevProps.getPause) {
+  //     clearInterval(this.interval)
+  //   }
+  // }
+  //
+  // componentWillUnmount() {
+  //   const { timeLeft } = this.props
+  //   clearInterval(this.interval)
+  //   clearInterval(setInterval(timeLeft, 5000))
+  // }
+
+  // render() {
+  //   const { onDeleted, onCompleted, done, onEdited, title, id, seconds, minutes, date } = this.props
+  //   const sec = seconds.toString().padStart(2, '0')
+  //   const min = minutes.toString().padStart(2, '0')
 
     return (
       <div className='view'>
-        <input type='checkbox' className='toggle' onChange={onCompleted} checked={done} />
+        <input type='checkbox' className='toggle' onChange={() => onCompleted()} checked={done} />
         <label htmlFor={id}>
           <span className='title'>{`${title}`}</span>
           <div className='description'>
-            <button className='icon icon-play' onClick={this.onStart}></button>
-            <button className='icon icon-pause' onClick={this.onPause}></button>
-            <div>{`${min}:${sec}`}</div>
+            <button className='icon icon-play' onClick={() => onStart()}></button>
+            <button className='icon icon-pause' onClick={()=> onPause()}></button>
+            <div>{`${minutes}:${seconds}`}</div>
           </div>
           <span className='description'>{`created ${date} ago`}</span>
         </label>
-        <button className='icon icon-edit' onClick={onEdited}></button>
-        <button className='icon icon-destroy' onClick={onDeleted}></button>
+        <button className='icon icon-edit' onClick={() => onEdited()}></button>
+        <button className='icon icon-destroy' onClick={() => onDeleted()}></button>
       </div>
     )
-  }
+  // }
 }
+export default Task
 
 Task.defaultProps = {
   onCompleted: () => {},
